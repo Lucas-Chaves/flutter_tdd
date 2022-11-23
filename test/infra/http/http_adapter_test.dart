9dd51a967e7f3ddbfe85663b547d2c838bb8b9b8
Lucas Chaves <lucas.limachaves@outlook.com>
2022-11-23 20:19:06 -0300

    ensure HttpAdapter returns data if post returns 200 with no data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -31,7 +31,7 @@ class HttpAdapter implements HttpClient {
       headers: headers,
       body: jsonBody,
     );
-    return jsonDecode(response.body);
+    return response.body.isEmpty ? {} : jsonDecode(response.body);
   }
 }
 
@@ -91,7 +91,7 @@ void main() {
         ),
       );
     });
-    test('Should return data if post returns 200', () async {
+    test('Should return data if post returns 200 with null data', () async {
       when(client.post(any, headers: anyNamed('headers'))).thenAnswer(
         (_) async => Response('{"any_key":"any_value"}', 200),
       );
@@ -101,6 +101,17 @@ void main() {
       );
 
       expect(response, {"any_key": "any_value"});
+    });
+    test('Should return data if post returns 200', () async {
+      when(client.post(any, headers: anyNamed('headers'))).thenAnswer(
+        (_) async => Response('', 200),
+      );
+      final response = await sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      expect(response, {});
     });
   });
 }
